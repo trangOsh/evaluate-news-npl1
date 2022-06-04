26b1ca47405cc26e2ae2cec704e72c94c4bb2646
trang oishi <trangthaidiep@gmail.com>
2022-06-04 00:33:32 -0700

    sucessful add API

--- __test__/testFormHandler.spec.js
+++ __test__/testFormHandler.spec.js
@@ -1,8 +1,9 @@
 import {handleSubmit} from '../src/client/js/formHandler';
+import {updateUI} from '../src/client/js/formHandler';
 //syntax is directly from the lecture
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
-describe("Testing the  url function is have its functionality", () => {
+describe("Testing the  handle submit function is worling", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
     test("Testing the handleSubmit() function", async() => {
         // Define the input for the function, if any, in the form of variables/array
@@ -10,3 +11,13 @@ describe("Testing the  url function is have its functionality", () => {
         
         expect(handleSubmit).toBeDefined();
     })});
+
+
+    describe("Testing the  UI function to see if it is working", () => {
+        // The test() function has two arguments - a string description, and an actual test as a callback function.  
+        test("Testing the upDateUI() function", async() => {
+            // Define the input for the function, if any, in the form of variables/array
+            // Define the expected output, if any, in the form of variables/array
+            
+            expect(updateUI).toBeDefined();
+        })});   
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,31 @@
+//const baseURL = "https://api.meaningcloud.com/sentiment-2.1?key=";
+
 function handleSubmit(event) {
     event.preventDefault()
-
+    const baseURL = "https://api.meaningcloud.com/sentiment-2.1?key=";
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
+    fetch('http://localhost:8081/apikey')
+    .then(res => res.text())
     .then(function(res) {
-        console.log(res)
-        document.getElementById('results').innerHTML = res.message
+        //console.log(res)
+        //document.getElementById('results').innerHTML = res.message
+        console.log(baseURL + res + "&of=json&url=" + formText +"&lang=en")
+       const apiSource = fetch(baseURL + res + "&of=json&url=" + formText +"&lang=en")
+        .then(res=> res.json())
+        .then(function(res){
+            console.log("ERRRR",res)
+            document.getElementById("agreement").innerHTML="Agreement: " + res.agreement;
+            document.getElementById("confidence").innerHMTL= "Confidence: " + res.confidence;
+            document.getElementById("irony").innerHTML ="Irony: "+  res.irony;
+            document.getElementById("model").innerHTML ="Model: " + res.model;
+            document.getElementById("subjectivity").innerHTML ="Subjectivity: " + res.subjectivity;
+        })
+       //console.log(apiSource)
+      // document.getElementById("agreement").innerH= "huhhahahahah";
     })
     
 }
